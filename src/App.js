ab49e9f5ad1ad1dde42f0292ddb5e85ea5c3b975
Shubham <samaroraytb@gmail.com>
2023-04-24 11:36:58 +0000

    useEffect Basics

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Scoreboard from './components/Scoreboard'
+
+import {GlobalStyle} from './styledComponents'
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Scoreboard />
+  </>
+)
+
+export default App
